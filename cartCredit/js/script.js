59a72e846863430ee1fd2961d40808ec3dd2279a
Dmitriy <dmitriy.taran1@gmail.com>
2020-09-24 18:03:05 +0300

    add page full-blog

--- cartCredit/js/script.js
+++ cartCredit/js/script.js
@@ -52,5 +52,79 @@ $(document).ready(function () {
             },
         }
     });
+
+    $('.carousel').carousel({
+        interval: false,
+    })
+
+
+    $('.load-more').on('click', function(e) {
+        e.preventDefault();
+        const btn = $(this);
+        const loader = btn.find('span');
+        const row = '<tr class="rating__row">\n' +
+            '                    <td class="rating__colum">\n' +
+            '                        <div class="rating__img rating__inner">\n' +
+            '                            <picture><source srcset="img/logo/citibank.webp" type="image/webp"><img src="img/logo/citibank.png" alt="" class="img-fluid"></picture>\n' +
+            '                        </div>\n' +
+            '                    </td>\n' +
+            '                    <td class="rating__colum">\n' +
+            '                        <div class="rating__rate-box rating__inner  rate-box">\n' +
+            '                            <span class="rating-box__item1 rating-box__item1_rate">Ставка</span>\n' +
+            '                            <span class="rating-box__item2 icon">от 6.5%</span>\n' +
+            '                            <span class="rating-box__item3 icon">Универсальный</span>\n' +
+            '                        </div>\n' +
+            '                    </td>\n' +
+            '                    <td class="rating__colum">\n' +
+            '                        <div class="rating__sum-box rating__inner sum-box">\n' +
+            '                            <span class="rating-box__item1 rating-box__item1_sum">Сумма и срок</span>\n' +
+            '                            <span class="rating-box__item2">50 000 ₽ - 3 000 000 ₽</span>\n' +
+            '                            <span class="rating-box__item3">На срок до 5 лет</span>\n' +
+            '                        </div>\n' +
+            '                    </td>\n' +
+            '                    <td>\n' +
+            '                        <div class="ratting__button-box col-md-12 rating__inner">\n' +
+            '                            <a class="btn ratting__btn orange-btn ratting__btn-1" href="#" role="button">Оставить заявку</a>\n' +
+            '                            <a class="btn ratting__btn  transparent-btn" href="#" role="button">Подробнее</a>\n' +
+            '                        </div>\n' +
+            '                    </td>\n' +
+            '                </tr>';
+        btn.attr('disabled', true);
+        loader.addClass('d-inline-block');
+        setTimeout(function(){
+            for (var i = 0; i < 4; i++) {
+                $('.rating__table').append(row);
+            }
+            loader.removeClass('d-inline-block');
+            btn.attr('disabled', false);
+        }, 1000);
+
+        // $.ajax({
+        //     url: '/data.html',
+        //     type: 'GET',
+        //     crossDomain: true,
+        //     dataType: "html",
+        //     beforeSend: function(){
+        //         btn.attr('disabled', true);
+        //         loader.addClass('d-inline-block');
+        //     },
+        //     success: function(response){
+        //         setTimeout(function(){
+        //             loader.removeClass('d-inline-block');
+        //             btn.attr('disabled', false);
+        //             $('after-posts').before(response);
+        //         }, 1000);
+        //     },
+        //     error: function(){
+        //         alert('Error!!!!!');
+        //         loader.removeClass('d-inline-block');
+        //         btn.attr('disabled', false);
+        //     }
+        // });
+    });
 });
+
+
+
+
 
